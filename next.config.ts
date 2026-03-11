fb4374dafb16740cfd08ecaba9d9bbddb5b17447
mkb2001 <kevinmugarura051@gmail.com>
2026-03-11 15:01:36 +0300

    feat: set runtime for middleware and remove experimental nodeMiddleware configuration

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
-  experimental: {
-    nodeMiddleware: true,
-  },
   images: {
     remotePatterns: [
       {
